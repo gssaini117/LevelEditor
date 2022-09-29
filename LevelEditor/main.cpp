622ad8e173cdfe7b23af03011b9a5d98ba349245
gssaini117 <82361767+gssaini117@users.noreply.github.com>
2022-09-29 11:30:51 -0700

    followed in-class tutorial

--- LevelEditor/main.cpp
+++ LevelEditor/main.cpp
@@ -6,26 +6,64 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
 
+using namespace sf;
+
+void handleInput(RenderWindow& window, Event& e);
+void update(RenderWindow& window);
+void render(RenderWindow& window);
+
+Music bgm;
+SoundBuffer sfxBuffer;
+Sound sfx;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    RenderWindow window(sf::VideoMode(800, 600), "Level Editor");
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    // load music
+    if (!bgm.openFromFile("Assets/Audio/BGM.wav")) {
+        // error
+        return -1;
+    }
+    bgm.play();
+    bgm.setLoop(true);
 
+    // load sfx
+    if (!sfxBuffer.loadFromFile("Assets/Audio/Victory Jingle SFX.wav")) {
+        // error
+        return -1;
+    }
+    sfx.setBuffer(sfxBuffer);
+
+    // game loop
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            handleInput(window, event);
         }
+        
+        if (event.key.code == Keyboard::Space)
+            sfx.play();
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        update(window);
+        render(window);
     }
 
     return 0;
+}
+
+void handleInput(RenderWindow& window, Event& e) {
+    if (e.type == sf::Event::Closed)
+        window.close();
+}
+
+void update(RenderWindow& window) {
+
+}
+
+void render(RenderWindow& window) {
+    window.clear();
+    window.display();
 }
