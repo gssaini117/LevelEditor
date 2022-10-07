020301273e38b8fb6d75e2df4fa44bc73ffa1340
gssaini117 <82361767+gssaini117@users.noreply.github.com>
2022-10-07 16:54:44 -0700

    able to load texture files

--- LevelEditor/main.cpp
+++ LevelEditor/main.cpp
@@ -5,35 +5,26 @@
 #include <SFML/System.hpp>
 #include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
+#include <iostream>
 
+using namespace std;
 using namespace sf;
 
+// prototypes
+void loadTileset();
 void handleInput(RenderWindow& window, Event& e);
 void update(RenderWindow& window);
 void render(RenderWindow& window);
 
-Music bgm;
-SoundBuffer sfxBuffer;
-Sound sfx;
+// globals
+const int NUM_OF_TILES = 21;
+//Image IMAGES[NUM_OF_TILES];
+Texture TILES[NUM_OF_TILES];
 
 int main()
 {
-    RenderWindow window(sf::VideoMode(800, 600), "Level Editor");
-
-    // load music
-    if (!bgm.openFromFile("Assets/Audio/BGM.wav")) {
-        // error
-        return -1;
-    }
-    bgm.play();
-    bgm.setLoop(true);
-
-    // load sfx
-    if (!sfxBuffer.loadFromFile("Assets/Audio/Victory Jingle SFX.wav")) {
-        // error
-        return -1;
-    }
-    sfx.setBuffer(sfxBuffer);
+    RenderWindow window(sf::VideoMode(1050, 700), "Level Editor");
+    loadTileset();
 
     // game loop
     while (window.isOpen())
@@ -43,9 +34,6 @@ int main()
         {
             handleInput(window, event);
         }
-        
-        if (event.key.code == Keyboard::Space)
-            sfx.play();
 
         update(window);
         render(window);
@@ -54,9 +42,21 @@ int main()
     return 0;
 }
 
+// Loads all tile textures
+void loadTileset() {
+    for (int i = 0; i < sizeof(TILES) / sizeof(Texture); i++) {
+        /*IMAGES[i].loadFromFile("Tileset/70x70/Platformer-" + to_string(i) + ".png");
+        TILES[i].loadFromImage(IMAGES[i]);*/
+        TILES[i].loadFromFile("Tileset/70x70/Platformer-" + to_string(i) + ".png");
+        cout << "Loaded Platformer-" << i << ".png" << endl;
+    }
+}
+
 void handleInput(RenderWindow& window, Event& e) {
     if (e.type == sf::Event::Closed)
         window.close();
+
+
 }
 
 void update(RenderWindow& window) {
